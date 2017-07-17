5b55a17f57d7812db2d775e53e5ea7aaacda5a40
Raul Ferreira <raulmendesferreira@gmail.com>
2017-07-17 01:18:11 +0100

    Moved to swift 4.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,11 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
-    name: "TicTacToe"
+    name: "TicTacToe",
+    targets: [
+        .target(
+            name: "TicTacToe")
+    ]
 )
